7c8301a23ccfa11b2784e4cf20a51e81b86e36de
See <407081305@qq.com>
2026-01-14 13:09:12 +0000

    Finetune with lora (#2)
    * update scripts
    
    * the final ver
    
    ---------
    
    Co-authored-by: Xi Chen <407083105@qq.com>

--- scripts/eval_our_finetuned_libero_spatial.sh
+++ scripts/eval_our_finetuned_libero_spatial.sh
@@ -2,6 +2,6 @@ cd $OPENVLA_REPO
 export MUJOCO_GL=glx
 python experiments/robot/libero/run_libero_eval.py \
   --model_family openvla \
-  --pretrained_checkpoint $OPENVLA_REPO/experiments/finetune_libero_spatial/openvla-7b+libero_spatial_no_noops+b4+lr-5e-05+lora-r32+dropout-0.0 \
+  --pretrained_checkpoint $OPENVLA_REPO/experiments/finetune_libero_spatial/openvla-7b+libero_spatial_no_noops+b4+lr-0.0001+lora-r32+dropout-0.0 \
   --task_suite_name libero_spatial \
   --center_crop True
--- start_headless.sh
+++ start_headless.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 """Usage: 
-  PRETRAINED_CHECKPOINT_ROOT=/home/xx/Documents/repos/hf_checkpoint \
-  OPENVLA_REPO=/home/xx/Documents/repos/openvla \
-  ./start_headless.sh
+PRETRAINED_CHECKPOINT_ROOT=/home/xx/Documents/repos/hf_checkpoint \
+OPENVLA_REPO=/home/xx/Documents/repos/openvla \
+FINETUNE_DATASET_DIR=/home/xx/Documents/repos/hf_dataset \
+./start_headless.sh
 """
 
-# current folder as WORD_DIR
+# current folder as WORK_DIR
 CURRENT_DIR=$(pwd)
 
 set -eo pipefail
@@ -24,6 +25,14 @@ if [ -z "$PRETRAINED_CHECKPOINT_ROOT" ]; then
 else
     echo "using PRETRAINED_CHECKPOINT_ROOT='$PRETRAINED_CHECKPOINT_ROOT'"
 fi
+# Finetune data mounts
+echo "using FINETUNE_DATASET_DIR='$FINETUNE_DATASET_DIR'"
+if [ -z "$FINETUNE_DATASET_DIR" ]; then
+    echo "You need to set \$FINETUNE_DATASET_DIR eg. FINETUNE_DATASET_DIR=~/finetune_data"
+    exit 1
+else
+    echo "using FINETUNE_DATASET_DIR='$FINETUNE_DATASET_DIR'"
+fi
 
 docker run -itd --name openvla \
     --gpus all \
@@ -31,8 +40,10 @@ docker run -itd --name openvla \
     --network=host \
     --privileged \
     --entrypoint /root/workspace/main/entrypoint.sh \
+    --shm-size=16G \
     -v $PRETRAINED_CHECKPOINT_ROOT:/root/checkpoint:rw \
     -v $OPENVLA_REPO:/root/openvla:rw \
     -v $CURRENT_DIR:/root/workspace/main:rw \
+    -v $FINETUNE_DATASET_DIR:/root/finetune_data:rw \
     -w /root/workspace/main \
     openvla:latest
